8871e9c8bda9bdb4d3a739c7ab2f3be1b99cdad5
jclin <jiancheng@gmail.com>
2022-08-22 16:23:27 -0700

    Update skip_ad.sh
    check for video buffering and the title is still unknown

--- skip_ad.sh
+++ skip_ad.sh
@@ -3,6 +3,6 @@
 DEVICE=<DEVICE_ID_in_go-chromecast>
 while :
 do
-go-chromecast -u ${DEVICE} status | grep BUFFERING | grep -v 'remaining=0s/0s, ' | grep -oP 'remaining=[^0]*s/' && go-chromecast -u ${DEVICE} skipad
-sleep 2
+go-chromecast -u ${DEVICE} status | grep BUFFERING | grep -v ', unknown,' | grep -oP 'remaining=[^0]*s/' && go-chromecast -u ${DEVICE} skipad
+sleep 1
 done
