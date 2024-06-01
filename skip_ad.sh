46f70a17b48d522dcc908dfea1adafb8b3cec04d
jclin <jiancheng@gmail.com>
2024-06-01 08:21:29 -0700

    Update skip_ad.sh

--- skip_ad.sh
+++ skip_ad.sh
@@ -1,8 +1,23 @@
-#!/bin/sh -f
+#!/bin/bash -f
 
-DEVICE=<DEVICE_ID_in_go-chromecast>
+# replace your chromecast device UUID and IP below
+DEVICE="-u <UUID> -a <IP>"
+CHROMCAST="timeout 5 go-chromecast ${DEVICE} status"
+RESULT=$($CHROMCAST)
 while :
 do
-go-chromecast -u ${DEVICE} status | grep BUFFERING | grep -v ', unknown,' | grep -oP 'remaining=[^0]*s/' && go-chromecast -u ${DEVICE} skipad
-sleep 1
+        echo $RESULT
+        echo $RESULT | grep BUFFERING | grep -v Peacock | grep -v unknown | grep -oP 'remaining=[^0]*s/' && go-chromecast ${DEVICE
+} skipad
+        while :
+        do
+                if [[ $RESULT == *"Backdrop"* ]]; then
+                        sleep 10
+                else
+                        break
+                fi
+                RESULT=$($CHROMCAST)
+        done
+        sleep 1
+        RESULT=$($CHROMCAST)
 done
